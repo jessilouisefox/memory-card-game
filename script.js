08d7b5a5f233ff5ebac0c39bae6d858e70fbff28
Jessica Garcia <jessica@Jessicas-MacBook-Pro.local>
2022-10-16 22:57:14 -0400

    Adds a reset board button

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ let matchedPairs = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
 
-function flipCard() {
-  console.log("flipCard was executed");
-}
-
-function shuffleCards() {}
-
 matchedPairs = 0; // reset matchedPairs variable to 0
 disableDeck = false; // reset disableDeck boolean to false
 cardOne = cardTwo = ""; // reset cardOne and cardTwo variables to empty string
@@ -42,9 +36,11 @@ function shuffleCards() {
 }
 
 shuffleCards();
+// reset cards for new game
+const resetBtn = document.querySelector('.reset-button')
 
+resetBtn.addEventListener('click', shuffleCards )
 function flipCard(evt) {
-  // take an event object's as a scoped variable
   const clickedCard = evt.target; // set the event's target DOM element as a variable
   if (cardOne !== clickedCard && !disableDeck) {
     // make sure that the current variable cardOne is not the same value as the clickedCard, AND that the deck is NOT disabled
@@ -94,4 +90,3 @@ function matchCards(img1, img2) {
     return;
   }, 1200);
 }
-
